869932b8618597f3fd872caab606679a132cb6fb
StevenSantos <steven.santos001@umb.edu>
2016-07-27 16:55:31 -0400

    Update Plotting_VandeCapelle.m
    Function ONLY for 1/3 wavelength slots @ 300E6 Hz.

--- Parametric-Analysis/Mutual-coupling/Plotting_VandeCapelle.m
+++ Parametric-Analysis/Mutual-coupling/Plotting_VandeCapelle.m
@@ -1,9 +1,17 @@
-function Plotting_VandeCapelle(max_seperation, frequency)
+function Plotting_VandeCapelle( max_seperation, freq)
 %Plot of Van de Capelle Code for Mutual Admittance
 
-lambda = 3E8/frequency;
+lambda = 3E8/freq;
 dy = (1/50)*lambda;
 Max_sep = max_seperation*lambda;
+
+%Constants that are untouched
+w_sub = 1.12;
+h_sub = 0.04;
+L_sub = 1.12;
+eps1 = 1;
+eps2 = 2.2;
+
 
 %Matrix of different seperation_distances----------------------------------
 %Seperation Matrix's first data point is dy, and last point is not 0
@@ -17,8 +25,8 @@ end
 %Matrix to fill with mutual admittance values------------------------------
 Admittance_Matrix = zeros(1, length(Separation_distance_matrix));
 index = 1;
-for L_ant = 2*dy:dy:Max_sep+dy
-    Admittance_Matrix(1,index) = HISantYmatSS(L_ant);
+for L_ant1 = 2*dy:dy:Max_sep+dy
+    Admittance_Matrix(1,index) = HISantYmat_SS(.333, dy, L_ant1,eps1, w_sub, h_sub, L_sub,eps2, freq);
     index = index + 1;
 end
 
@@ -39,4 +47,3 @@ plot(Separation_distance_matrix,Real_matrix)%, Separation_distance_matrix, Imagi
 legend('Real Part')%, 'Imaginary Part')
 xlabel('Slot Separation')
 ylabel('Mutual Admittance')
-
