e19e18ce82df9921f63c73d89d8c28bc074a5cff
Kevin Keegan <kevin.keegan001@umb.edu>
2016-09-16 17:23:54 -0400

    updated comments

--- Calibration/generalized_S.m
+++ Calibration/generalized_S.m
@@ -1,14 +1,16 @@
 % Function takes S-parameters read in from HFSS and converts them into
 % generalized scattering parameter matrices. The end result is a 2x2 matrix
-% composed of smaller submatrices of size "elements".
+% composed of smaller submatrices each with size of sq_size/2.
 
-function [S11, S12, S21, S22, sub_size] = generalized_S(M,depth,sq_size)
+function [S11, S12, S21, S22, S] = generalized_S(M,depth,sq_size)
 
-% Reshapes the data from HFSS into a square matrix of size "dimension".
+% Reshapes the data from HFSS into a square matrix of size "sq_size".
+% Retains the sq_size variable so this produces square S matrices of size 
+% 2n x 2n, n = 1,2,...,etc. Submatrices (S11,S12,etc) are of size n x n.
 
 S = zeros(sq_size,sq_size,depth);
 for ii = 1:depth
-    row = M(ii, :);
+    row = M(ii,:);
     square = reshape(row, sq_size, sq_size);
     S(:,:,ii) = square;
 end
@@ -17,19 +19,18 @@ end
 % "elements".
 
 if sq_size > 2
-    sub_size = (sq_size)/2;
+    sub_size = sq_size/2;
     S11 = zeros(sub_size,sub_size,depth);
     S12 = zeros(sub_size,sub_size,depth);
     S21 = zeros(sub_size,sub_size,depth);
     S22 = zeros(sub_size,sub_size,depth);
     for ii = 1:depth
         S11(:,:,ii) = S(1:sub_size,1:sub_size,ii);
-        S12(:,:,ii) = S(1:sub_size,sub_size+1:end,ii);
-        S21(:,:,ii) = S(sub_size+1:end,1:sub_size,ii);
-        S22(:,:,ii) = S(sub_size+1:end,sub_size+1:end,ii);
+        S12(:,:,ii) = S(1:sub_size,sub_size+1:sq_size,ii);
+        S21(:,:,ii) = S(sub_size+1:sq_size,1:sub_size,ii);
+        S22(:,:,ii) = S(sub_size+1:sq_size,sub_size+1:sq_size,ii);
     end
 else 
-    sub_size = 1;
     S11 = zeros(1,1,depth);
     S12 = zeros(1,1,depth);
     S21 = zeros(1,1,depth);
@@ -41,6 +42,3 @@ else
         S22(1,1,ii) = S(2,2,ii);
     end
 end
-
-
-
