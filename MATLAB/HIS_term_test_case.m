f12392820cc6147a1bfd0f8f3d3badfdedd2b8d1
kckerbypatel <kc.kerby-patel@umb.edu>
2017-04-23 17:26:48 -0400

    finished HIS_term_test_case
    has no mutual impedance/admittance terms - left and right halves of
    antenna are terminated by two separate 2x2 load impedance matrices.

--- MATLAB/HIS_term_test_case.m
+++ MATLAB/HIS_term_test_case.m
@@ -1,4 +1,4 @@
-function [ Zin ] = HIS_term_test_case( n, a, w1, w2, h1, h2, via_rad, eps1, eps2, f, feed, ZL)
+function [ Zin ] = HIS_term_test_case( n, a, w1, w2, h1, h2, via_rad, eps1, eps2, f, feed, ZL_left, ZL_right)
 %calculates input impedance of a terminated HIS structure ("antenna" and
 %HIS have the same length so there's no HIS-only section).  ZL is a vector
 %(no mutual impedances) that describes the termination.  ZL1 is left upper
@@ -14,16 +14,26 @@ unitcell = multicond_unitcell(a, w1, w2, h1, h2, via_rad, eps1, eps2, f, 1); %us
 
 MTLhalf=unitcell^(n/2);
 
-Zleft = terminate_ABCD4(MTLhalf, [ZL(1), 0; 0, ZL(2)]);
+Zleft = terminate_ABCD4(MTLhalf, ZL_left);
 
-Zright = terminate_ABCD4(MTLhalf, [ZL(3), 0; 0, ZL(4)]);
+Zright = terminate_ABCD4(MTLhalf, ZL_right);
 
 if feed==1 %probe feed
     %boundary conditions are that upper Vleft = Vright=V, lower Vleft =
     %Vright, lower Ileft = -Iright, upper Ileft+Iright=I
+    YL = inv(Zleft);
+    YR = inv(Zright);
+    
+    Yin = YL(1,1)+YR(1,1)-(YL(1,2)+YR(1,2))*(YL(2,1)+YR(2,1))/(YL(2,2)+YR(2,2));
+    Zin =1/Yin;
+    
 else %diff feed
     %boundary conditions are that upper Vleft-Vright=V, upper Ileft=Iright,
     %lower Vleft=Vright, lower Ileft = Iright
+    
+    Zin = Zleft(1,1)-Zright(1,1)+(Zleft(1,2)+Zright(1,2))*(Zleft(2,1)-Zright(2,1))/(Zleft(2,2)+Zright(2,2));
+    
+    
 end
 
 end
