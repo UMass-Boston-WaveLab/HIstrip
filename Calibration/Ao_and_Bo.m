9c1f25b96f7906d18ed21e0290e0830248917545
KevinKeegan <keegan.k@husky.neu.edu>
2017-06-16 17:02:15 -0400

    cleaned up formatting/comments

--- Calibration/Ao_and_Bo.m
+++ Calibration/Ao_and_Bo.m
@@ -1,17 +1,15 @@
 % Calculates the error boxes Ao and Bo from the measured
 % thru and line standards. 
 
-function[invAo,Bo] = Bo(Ao,tt,thrulength,sorted_prop2,depth)
+function[invAo, Bo] = Bo(Ao,tt,thrulength,sorted_prop2,depth)
 
 % Preallocating and inverting Ao matrix.
-
 invAo = zeros(4,4,depth);
 for ii = 1:depth
     invAo(:,:,ii) = inv(Ao(:,:,ii));
 end
 
 % Calculates the reverse cascaded product of inv(Ao) and M1.
-
 Z = zeros(4,4,depth);
 for ii = 1:depth
     Z(:,:,ii) = invAo(:,:,ii)*tt(:,:,ii);
@@ -20,7 +18,6 @@ YoM1 = reverse_cascade(Z);
 
 % Calculates the known T-parameters of the thru standard using the
 % previously determined propagation constants, and permutates the matrix.
-
 N = zeros(4,4,depth);
 for ii = 1:depth
     for jj = 1:4
@@ -30,7 +27,6 @@ end
 NN = permutate(N);
 
 % Calculates Bo.
-
 Bo = zeros(4,4,depth);
 for ii = 1:depth
     Bo(:,:,ii) = YoM1(:,:,ii)*NN(:,:,ii);
