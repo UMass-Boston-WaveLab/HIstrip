cbf39d6c59dc659dedb75cb092a3351d0099f3a8
KevinKeegan <keegan.k@husky.neu.edu>
2017-06-16 17:14:41 -0400

    cleaned up formatting/comments

--- Calibration/Nxo.m
+++ Calibration/Nxo.m
@@ -3,10 +3,8 @@
 function[Nxo] = Nxo(Ao,Bo,Ko,dutT,depth)
 
 % Calculates the preliminary matrices to get Nxo.
-
 invAo = zeros(4,4,depth);
 invKo = zeros(4,4,depth);
-
 for ii = 1:depth
     invAo(:,:,ii) = inv(Ao(:,:,ii));
     invKo(:,:,ii) = inv(Ko(:,:,ii));
@@ -15,7 +13,6 @@ end
 permBo = permutate(Bo);
 
 % Calculates the Nxo matrix. See eqn. 53 in the multimode TRL paper.
-
 Nxo = zeros(4,4,depth);
 
 for ii = 1:depth
