f714a7c59085f8af6fe54ecd443553f5c74b3292
Joe Dusenbury <joseph.dusenbury001@umb.edu>
2017-08-07 19:14:56 -0500

    Org

---
+++ MATLAB/Function Library/seriesHISstub.m
@@ -0,0 +1,16 @@
+function [ Zin ] = seriesHISstub( f, w1, w2, h_ant, h2, rad, eps1,eps2, g, L1,L2)
+%SERIESHISSTUB finds the two-terminal (upper to ground) input impedance of a
+%combination of two HIS-backed stubs in series (i.e. differential feed like
+%a dipole)
+
+
+Zin1 = HISstub(f, w1, w2, h_ant, h2, rad, eps1,eps2, g, L1,0, 4*0.14,8*0.14,1);
+Zin2 = HISstub(f, w1, w2, h_ant, h2, rad, eps1,eps2, g, L2,0, 4*0.14,8*0.14,1);
+
+for ii=1:length(f)
+    Y=inv(Zin1(:,:,ii)+Zin2(:,:,ii));
+    
+    Zin(ii)=1/Y(1,1);
+end
+
+end
