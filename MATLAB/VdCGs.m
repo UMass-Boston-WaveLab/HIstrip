6f544c17fc555a4e2ba7926db72db26e1704e807
Joe Dusenbury <joseph.dusenbury001@umb.edu>
2017-08-07 19:20:15 -0500

    Moved to Function Lib

--- MATLAB/VdCGs.m
+++
@@ -1,7 +0,0 @@
-function [G] = VdCGs(w, s)
-%self admittance of a slot from van de capelle, dimensions in wavelengths.
-G = (1/(pi*377))*((w*sinint(w)+sin(w)/w+cos(w)-2)*(1-s^2/24) + ...
-    (s^2/12)*(1/3+cos(w)/w^2-sin(w)/w^3));
-end
-
-
