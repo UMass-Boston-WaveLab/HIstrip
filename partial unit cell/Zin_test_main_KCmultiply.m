59eb600f65c9755bbdf6e1c2064f32d44aea213d
ssoltani62 <30444582+ssoltani62@users.noreply.github.com>
2017-11-17 10:18:17 -0500

    ignore

--- partial unit cell/Zin_test_main_KCmultiply.m
+++ partial unit cell/Zin_test_main_KCmultiply.m
@@ -90,7 +90,7 @@ slot_4_x=w_sub;
 % L_ant_eff = L_ant+microstripdeltaL(w_ant, h_ant, eps1);
 
 [ABCD, ABCDgaphalf1,ABCDline,ABCDL,~] = HISlayerABCD(w2, g, H_sub, rad, eps2, f, viaflag, eps1);
-botn = floor((L_sub-L_ant_eff)/(2*a))-1;
+botn = floor((L_sub-L_ant_eff)/(2*a))-1;% Number of compelete unit cell not under antenna===HIS
 for ii = 1:length(f)
 
 %  Y(:,:,ii)= HIS_admittance_saber_test(sep_12, sep_13, sep_14, sep_23, sep_24, sep_34, slot_1_x, slot_2_x, slot_3_x, slot_4_x, f(ii));
@@ -108,9 +108,9 @@ ZLtemp=ZL;
 temp={ABCDgaphalf1(:,:,ii),ABCDline(:,:,ii),ABCDL(:,:,ii),ABCDline(:,:,ii)};
 
 for jj=length(temp):-1:1
-    ZLtemp = unitcellMultiply(ZLtemp, temp{jj}, 1);% LEFT last HIS connection fro ground side to load
+    ZLtemp = unitcellMultiply(ZLtemp, temp{jj}, 1);%  last HIS connection from ground side to load
 end
-ZinL_l = unitcellMultiply(ZLtemp, ABCD(:,:,ii), botn);% HIS from antenna edge to last HIS connection fro ground side
+ZinL_l = unitcellMultiply(ZLtemp, ABCD(:,:,ii), botn);% HIS from antenna edge to last HIS connection from ground side
 
 ZRtemp=ZR;
 for jj=length(temp):-1:1
@@ -126,7 +126,7 @@ unitcell=multicond_unitcell(a,  w_ant, w2, h_ant+H_sub, H_sub, rad, eps1, eps2, 
 ZLR=1/squeeze(Y(2,2,ii));
 ZLL=1/squeeze(Y(3,3,ii));
 
-N=floor(0.5*L_ant_eff/a); % NUMBER OF COKPLETE UNIT CELLS UNDER ANTENNA
+N=floor(0.5*L_ant_eff/a); % NUMBER OF COMPLETE UNIT CELLS UNDER ANTENNA
 
 %% NEED IF STATEMENT HERE
 ZinR_mid = partialcells([ZLR 0; 0 ZinR_l], L_ant_eff, a, w1, w2, H_sub+h_ant, H_sub, rad, eps1, eps2, f(ii), viaflag);
