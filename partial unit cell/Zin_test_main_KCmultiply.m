499d0db9fc59109b52caa8dedf637d027df30409
kckerbypatel <kc.kerby-patel@umb.edu>
2017-11-22 14:52:06 -0500

    resolve conflict

--- partial unit cell/Zin_test_main_KCmultiply.m
+++ partial unit cell/Zin_test_main_KCmultiply.m
@@ -25,11 +25,7 @@ a =w2+g;       %unit cell size
 
 L_sub = 16*a;
 w_sub = 16*a;
-<<<<<<< HEAD
 L_ant = 0.48; 
-=======
-L_ant = 4*a+a-g/2; 
->>>>>>> origin/partial-cells
 %f = 2e9:250e6:10e9; %f vector sweep for 6ghz
 %f = 1e9:500e6:6.5e9;
 f=(100:5:600)*10^6/sf;
