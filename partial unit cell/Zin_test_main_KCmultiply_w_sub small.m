b7699f2cded28d8950d0512d8f0db29b13e3782a
SABER.SOLTANI <30444582+ssoltani62@users.noreply.github.com>
2018-01-30 13:42:06 -0500

    ignore

--- partial unit cell/Zin_test_main_KCmultiply_w_sub small.m
+++ partial unit cell/Zin_test_main_KCmultiply_w_sub small.m
@@ -6,13 +6,13 @@ clc
 clear all;
 
 %sf = .05; %scale factor from 300Mhz to 6Ghz,,, ==Lamda
-% the best/hanna antenna is actually a 0.005m radius dipole
+% the best/han7362karamQ!na antenna is actually a 0.005m radius dipole
 %"the equivalent strip width for a cylindrical wire is almost twice (~1.89)
 %the wire diameter"
 % (see "Equivalent Strip Width for Cylindrical Wire for Mesh Reflector
 % Antennas: Experiments, Waveguide, and Plane-Wave Simulations")
 sf = 1; 
-w_ant = 0.0189*sf; %depends on kind of antenna placed on top of HIS
+w_ant = 0.01*sf; %depends on kind of antenna placed on top of HIS
 w1=w_ant;
 H_sub = 0.04*sf; %ground to patch distance
 h_ant = 0.02*sf; %antenna height above substrate
@@ -23,9 +23,8 @@ g = 0.02*sf;     %patch spacing
 gap=g;
 a =w2+g;       %unit cell size
 
-L_sub = 8*a;
-w_sub = 1*a;
-% w_sub = 2*a;
+L_sub = 16*a;
+w_sub = 16*a;
 
 L_ant = 0.48; 
 %f = 2e9:250e6:10e9; %f vector sweep for 6ghz
