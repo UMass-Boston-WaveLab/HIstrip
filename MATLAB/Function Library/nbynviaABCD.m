381de940f4be60d6af3c2c8f4831283c8f4012ad
kckerbypatel <kc.kerby-patel@umb.edu>
2018-03-05 14:24:48 -0500

    added generalized via ABCD matrix function
    makes ABCD matrix for 2n by 2n ABCD matrix.

---
+++ MATLAB/Function Library/nbynviaABCD.m
@@ -0,0 +1,21 @@
+function [ ABCD ] = nbynviaABCD( h2, via_rad, f, N )
+%NBYNVIAABCD calculates the 2n by 2n generalized ABCD matrix for a via in a
+%multiconductor transmission line representation of microstrip line above a
+%HIS.  The via is from the HIS layer to ground.
+
+
+L_via = viaL(h2, via_rad);
+
+omega=2*pi*f;
+
+A = eye(N);
+B = zeros(N);
+D = eye(N);
+C = (1/(j*omega*L_via))*eye(N);
+C(1,1)=0;
+
+ABCD = [A B; C D];
+
+
+end
+
