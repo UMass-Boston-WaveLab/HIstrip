a32dc747cca388ee7b089ec3c94d6e4e31d2a88e
SABER.SOLTANI <30444582+ssoltani62@users.noreply.github.com>
2018-03-09 14:16:54 -0500

    HFSS Cap values

--- MATLAB/Function Library/ustripMTLABCD.m
+++ MATLAB/Function Library/ustripMTLABCD.m
@@ -11,10 +11,18 @@ omega = 2*pi*f;
 [~, C12, L12, ~] = microstrip(w1, h1-h2, eps1); %I'm using microstrip per-unit-length capacitance values here
 [Z2, C2G, L2G, epseff2] = microstrip(w2, h2, eps2);
 
+C12=29e-12*sf;
+C2G=117e-12*sf;
+
 cap = [C12, -C12; -C12, C2G+C12]; %Symmetric; see MTL book for where this comes from
+% HFSS model results
 
 [~, C120, ~, ~] = microstrip(w1, h1-h2, 1); 
 [~, C2G0, ~, ~] = microstrip(w2, h2, 1);
+
+C120=29e-12*sf;
+C2G0=75e-12*sf;
+
 cap0 = [C120, -C120; -C120, C2G0+C120]; %symmetric
 
 %alternative option for calculating PUL capacitance - also not so hot
