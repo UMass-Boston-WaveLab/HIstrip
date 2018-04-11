4dcde2036ebe3f8c13f71d5aca1727b9b5b70245
ssoltani62 <30444582+ssoltani62@users.noreply.github.com>
2018-04-11 08:18:59 -0400

    done

--- MATLAB/nbynmain.m
+++ MATLAB/nbynmain.m
@@ -1,8 +1,9 @@
+close all;
 clc
 clear all;
 %% DEFINE PARAMETERS
 
-sf = 1/20; %scale factor
+sf = 1/1; %scale factor
 w_ant = 0.01*1.89*sf; %depends on kind of antenna placed on top of HIS
 w1=w_ant;
 h_sub = 0.04*sf; %ground to patch distance
@@ -33,11 +34,21 @@ viaflag = 1;
 E = eye(4);
 
 %% load/enter per unit length capacitance matrices here
-cap=(1e-12)*[33 -29; -29 117];
-cap0=(1e-12)*[33 -29; -29 75];
+
+% DATA FROM HFSS
+% cap=(1e-12)*[33 -29; -29 117];
+% cap0=(1e-12)*[33 -29; -29 75];
+% HIScap=117e-12;  %if we calculate the cap matrix with and without the top line 
+% HIScap0=75e-12; %and the HIS-related rows don't change, we may not need these 
+%             %to be calculated separately.
+
+% DATA FROM OUR CAP CALCULATOR
+cap=(1e-12)*[29 -29; -29 117];
+cap0=(1e-12)*[27 -27; -27 74];
 HIScap=117e-12;  %if we calculate the cap matrix with and without the top line 
 HIScap0=75e-12; %and the HIS-related rows don't change, we may not need these 
             %to be calculated separately.
+
 
 M=size(cap,1);  %minimum 2 - total number of non-GND conductors in multiconductor line including antenna layer
                 % this is up to the user - don't have to include all the HIS rows if
@@ -47,6 +58,27 @@ M=size(cap,1);  %minimum 2 - total number of non-GND conductors in multiconducto
 
 %% input impedance calculation steps
 for ii = 1:length(f)
+    
     Zin(ii)=nbynHIStripZin(w_ant, h_ant, L_ant,eps1, w2, h_sub, L_sub, eps2, a, g, rad, cap, cap0, HIScap, HIScap0, f(ii));
+    S11(ii) = (Zin(ii)-50)/(Zin(ii)+50);
 end
 %% make plots
+ figure; 
+plot(f*1e-9, real(Zin), f*1e-9, imag(Zin),'linewidth',2)
+xlabel('Frequency [GHz]')
+ylabel('Zin')
+legend({'R';'X'})
+grid on
+set(gca,'fontsize',14)    
+% xlim([0.1 0.6])
+% ylim([-400 800])
+
+
+
+figure; 
+plot(f*1e-9, 20*log10(abs(S11)), 'linewidth',2)
+xlabel('Frequency [GHz]')
+ylabel('|S_{11} (dB)')
+grid on
+set(gca,'fontsize',14)
+% xlim([0.1 0.6])
