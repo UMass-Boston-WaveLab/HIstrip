520497990a83bd0d81fadff1502b719e967c926f
SABER.SOLTANI <30444582+ssoltani62@users.noreply.github.com>
2018-05-21 12:28:45 -0400

    Revert "I put in the Z fix wrong"
    This reverts commit c58f94d52625605b11bf43fe8d1e34a1d85fb3bd.

--- MATLAB/Function Library/nbynpartialcells.m
+++ MATLAB/Function Library/nbynpartialcells.m
@@ -65,9 +65,8 @@ end
 Zin = [ zeros([size(ZLu,1),size(ZLb,2)+size(ZLu,2)]);
        zeros([size(ZLb,1),size(ZLu,2)]) ZLb];
 for ii=1:length(zluindex)  %this will break if ZLu isn't scalar but I don't know why it wouldn't be.
-    Zin(1,zluindex(ii)) = ZLb(zluindex, zluindex);
-    Zin(zluindex(ii),1) = ZLb(zluindex, zluindex);
-    Zin(1,1)=ZLu+ZLb(zluindex, zluindex); 
+    Zin(1,zluindex(ii)) = length(zluindex)*ZLu;
+    Zin(zluindex(ii),1) = length(zluindex)*ZLu;
 end
 %    
 % =======
