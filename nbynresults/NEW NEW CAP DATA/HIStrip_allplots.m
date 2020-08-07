90ca07c22648fced74c34fc0cf97f5af094bbdab
kckerbypatel <kc.kerby-patel@umb.edu>
2020-08-07 15:50:35 -0400

    Update HIStrip_allplots.m
    plot formatting improvements

--- nbynresults/NEW NEW CAP DATA/HIStrip_allplots.m
+++ nbynresults/NEW NEW CAP DATA/HIStrip_allplots.m
@@ -45,28 +45,13 @@ for row=nrows
     set(gca, 'PlotBoxAspectRatio', [1 0.5 0.5])
     
     ylim([-500 1000])
-    
-    % hold on
-    % x=hs1.Frequencies./10^9;
-    % plot(x,-10,'-k','LineWidth',.5);
+    xlim([100 450])
+
     set(gca,'fontsize',14,'fontweight','b','FontName','Times New Roman');
-    % title('Sine Function','fontsize',12,'fontweight','b','FontName','Times New Roman');
     xlabel('Frequency (MHz)','fontsize',14,'fontweight','b','FontName','Times New Roman');
     ylabel('Input Impedance(\Omega)','fontsize',14,'fontweight','b','FontName','Times New Roman');
     
-    % S11=S1(:,3);%3raw
-    % plot(Freq,S11,':r','LineWidth',3);
-    %
-    %
-    % hold on
-    %
-    % S11=S1(:,4);%5raw
-    % plot(Freq,S11,':g','LineWidth',3);
-    %
-    % hold on
-    %
-    
-    %S11_h=(S1(:,2)+j*S1(:,3)-50)./(S1(:,2)+j*S1(:,3)+50);
+
     S11_m=(Zin-50)./(Zin+50);
     
     Sdelta=abs(S11)-abs(S11_m).';
@@ -76,36 +61,41 @@ for row=nrows
     S11_m_log=20*log10(abs((Zin-50)./(Zin+50)));
     plot(Freq,S11_h_log,'k',f*1e-6, S11_m_log, '--k','LineWidth',2);
     hold on
-    
-    %legend('HFSS: 3 row','Model: 3 row');
+
     set(gca, 'PlotBoxAspectRatio', [1 0.5 0.5])
-    
-    % hold on
-    % x=hs1.Frequencies./10^9;
-    % plot(x,-10,'-k','LineWidth',.5);
+    grid on
+
     set(gca,'fontsize',14,'fontweight','b','FontName','Times New Roman');
-    % title('Sine Function','fontsize',12,'fontweight','b','FontName','Times New Roman');
     xlabel('Frequency (MHz)','fontsize',14,'fontweight','b','FontName','Times New Roman');
     ylabel('|S_{11}| (dB)','fontsize',14,'fontweight','b','FontName','Times New Roman');
-
+    xlim([100 450])
     
     
     
     figure
 
     plot(f/1e6, abs(Sdelta),'k','LineWidth',2);
-    
-    %legend('HFSS: 3 row','Model: 3 row');
-    set(gca, 'PlotBoxAspectRatio', [1 0.5 0.5])
-    
-    % hold on
-    % x=hs1.Frequencies./10^9;
-    % plot(x,-10,'-k','LineWidth',.5);
+
+    set(gca, 'PlotBoxAspectRatio', [1 0.5 0.5])    
     set(gca,'fontsize',14,'fontweight','b','FontName','Times New Roman');
-    % title('Sine Function','fontsize',12,'fontweight','b','FontName','Times New Roman');
     xlabel('Frequency (MHz)','fontsize',14,'fontweight','b','FontName','Times New Roman');
     ylabel('\Delta(S_{11})','fontsize',14,'fontweight','b','FontName','Times New Roman');
+    xlim([100 450])
     
+    sprintf('RMS delta S11 for %.0f row case: %.4f \n', rows, mean(Sdelta(Freq<=450).^2))
+    
+    if row==3
+        Zhfss=hs_hfss.data(:,2)+j*hs_hfss.data(:,3);
+        Freq = hs_hfss.data(:,1);
+        S11_h=(Zhfss-50)./(Zhfss+50);
+        load('8by1 new paper.mat')
+        
+        S11_1D = (Zd-50)./(Zd+50);
+        
+        Sdelta_1D= S11_h-S11_1D;
+
+        sprintf('1D model RMS delta S11 for %.0f row case: %.4f \n', rows, mean(Sdelta_1D(Freq<=450).^2))
+    end
     if row==7
         f7=f;
         Zin7=Zin;
@@ -120,19 +110,28 @@ for row=nrows
         hold on
         p4=plot(f*1e-6, imag(Zd), '-.','linewidth',2);
         p3.Color=[0.7 0.7 0.7];
-        p4.Color=[0.7 0.7 0.7];
-        S1=hs_hfss.data;
-        Freq = S1(:,1);
-        S11=S1(:,2);%1raw
-        plot(Freq,S11,'k','LineWidth',2);
-        
+        p4.Color=[0.7 0.7 0.7];        
+        Zhfss=hs_hfss.data(:,2)+j*hs_hfss.data(:,3);
+        Freq = hs_hfss.data(:,1);
+        S11_h=(Zhfss-50)./(Zhfss+50);
+        plot(Freq,real(Zhfss),'k','LineWidth',2);
         hold on
-        S11=S1(:,3);%1raw
-        plot(Freq,S11,':k','LineWidth',2);
+        plot(Freq,imag(Zhfss),':k','LineWidth',2);
         grid on
         set(gca, 'PlotBoxAspectRatio', [1 0.5 0.5])
-        ylim([-1000 1500])
-        %legend('Re. Model: 7 row','Im. Model: 7 row','Re. 1D Model','Im. 1D Model','Re. HFSS: 7 row','Im. HFSS: 7 row','location','northwest');
+        set(gca,'fontsize',14,'fontweight','b','FontName','Times New Roman');
         legend('Re. 1D Model','Im. 1D Model','Re. HFSS: 7 row','Im. HFSS: 7 row','location','northwest');
+        xlabel('Frequency (MHz)','fontsize',14,'fontweight','b','FontName','Times New Roman');
+        ylabel('Input Impedance(\Omega)','fontsize',14,'fontweight','b','FontName','Times New Roman');
+        ylim([-500 1000])
+        xlim([100 450])
+        
+
+        S11_1D = (Zd-50)./(Zd+50);
+        
+        Sdelta_1D= S11_h-S11_1D;
+
+        sprintf('1D model RMS delta S11 for %.0f row case: %.4f \n', rows, mean(Sdelta_1D(Freq<=450).^2))
+    
     end
 end
